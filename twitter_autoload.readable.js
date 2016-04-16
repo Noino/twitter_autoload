5aaeb48f49d670ce932c0ed9f1fc746713f49ca8
noino <willewendell@gmail.com>
2016-04-17 00:43:15 +0300

    - Fixed: Loading at the actual top of timeline, regardless of all the clutter twitter decides to put on top - TODO: Figure out a more obvious way to pause updating then focusing a text area incase the user wants to do something above the timeline

--- twitter_autoload.readable.js
+++ twitter_autoload.readable.js
@@ -1,5 +1,4 @@
-/*! Twitter autoload source | (c) 2014 William Wendell */
-
+/*! Twitter autoload source | (c) 2016 William Wendell */
 
 (function(){
 	'use strict';
@@ -22,7 +21,7 @@
 		
 		// observer config
 		this.oc = { attributes: false, childList: true, characterData: false };
-		this.tl = document.getElementById('timeline'); // timeline container
+		this.tl = document.getElementById('stream-items-id');
 		this.t0 = 2500; // initial polling delay
 
 		document.addEventListener('visibilitychange', 
@@ -34,7 +33,7 @@
 			}).bind(this), false
 		); 
 		this.o.observe(document.querySelector('.js-new-items-bar-container'), this.oc);
-		this.o2.observe(document.querySelector('#stream-items-id'), this.oc);
+		this.o2.observe(this.tl, this.oc);
 		
 		this.keepup();
 	};
@@ -98,10 +97,3 @@
 	
 	window.onload = window.setTimeout(new Autoload(), 5000);
 })();
-
-
-/*
-
-(function(){ 'use strict'; var Autoload = function(){ this.uq=false; this.v=!document.hidden; this.exp = -1; this.o = new MutationObserver((function(m){ this.up(); }).bind(this)); this.o2 = new MutationObserver((function(m){ this.undelay(); }).bind(this)); this.oc = { attributes: false, childList: true, characterData: false }; this.tl = document.getElementById('timeline'); this.t0 = 2500; document.addEventListener('visibilitychange', (function(ev){ if (this.v=!document.hidden && this.uq) { this.tk(); } }).bind(this), false ); this.o.observe(document.querySelector('.js-new-items-bar-container'), this.oc); this.o2.observe(document.querySelector('#stream-items-id'), this.oc); this.keepup(); }; Object.assign(Autoload.prototype, { su: function(px) { function getOffsetSum(e) { var top=0, left=0; while(e) { top = top + parseInt(e.offsetTop); left = left + parseInt(e.offsetLeft); e = e.offsetParent; } return {top: top, left: left}; } return !document.querySelectorAll('input:focus,textarea:focus,[contenteditable=\"true\"]:focus').length && window.pageYOffset - document.documentElement.clientTop - getOffsetSum(this.tl).top < px && this.v; }, tk: function() { if (this.su(15)) { document.dispatchEvent(new CustomEvent('uiShortcutGotoTopOfScreen')); this.uq = false; } else { if (this.v) { window.setTimeout( this.tk.bind(this), 100); } } }, up: function() { if (!this.uq) { this.uq = true; this.tk(); return true; } else { return false; } }, undelay: function() { this.exp = -1; }, delay: function() { if (this.exp < 10) { ++this.exp; } }, keepup: function() { if (this.up()) { this.delay(); } window.setTimeout(this.keepup.bind(this), parseInt(this.t0 * Math.pow(1.3, this.exp)) ); } }); window.onload = window.setTimeout(new Autoload(), 5000); })(); 
-
-*/
